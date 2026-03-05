5ffb0860f4dcccc2d048250cf1c1f14afcbe53ec
Aalok Tanwar <aaloktanwar88@gmail.com>
2026-03-05 13:36:39 +0530

    rate-limiter integration test added

--- integration-tests/src/test/java/PatientIntegrationTest.java
+++ integration-tests/src/test/java/PatientIntegrationTest.java
@@ -1,7 +1,9 @@
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +15,42 @@ public class PatientIntegrationTest {
 
     @Test
     public void shouldReturnPatientsWithValidToken () {
+        String token = getToken();
+
+        given()
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get("/api/patients")
+                .then()
+                .statusCode(200)
+                .body("patients", notNullValue());
+    }
+
+    @Test
+    public void shouldReturn429AfterLimitExceeded () throws InterruptedException {
+        String token = getToken();
+        int total = 10;
+        int tooManyRequest = 0;
+
+        for(int i = 1; i <= total; i++){
+            Response response = RestAssured
+                    .given()
+                    .header("Authorization", "Bearer " + token)
+                    .get("/api/patients");
+
+            System.out.printf("Request %d -> Status: %d%n", i, response.statusCode());
+
+            if(response.statusCode() == 429){
+                tooManyRequest++;
+            }
+
+            Thread.sleep(100);
+        }
+
+        assertTrue(tooManyRequest >= 1, "Expected at least 1 request to be rate limited (429)");
+    }
+
+    private static String getToken() {
         String loginPayload = """
           {
             "email": "admin@test.com",
@@ -30,13 +68,6 @@ public class PatientIntegrationTest {
                 .extract()
                 .jsonPath()
                 .get("token");
-
-        given()
-                .header("Authorization", "Bearer " + token)
-                .when()
-                .get("/api/patients")
-                .then()
-                .statusCode(200)
-                .body("patients", notNullValue());
+        return token;
     }
 }
